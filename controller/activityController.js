c0fd113a7a06ae14c08b143d4250addda36ffc93
RiccardoParrino <riccardoparrino1@gmail.com>
2025-09-29 12:15:56 +0200

    adding some services

--- controller/activityController.js
+++ controller/activityController.js
@@ -1,15 +1,21 @@
+const activityService = require('../service/activityService');
+
 exports.createActivity = (req, res, next) => {
+    console.log(activityService.createActivity(req));
     res.send("CREATE ACTIVITY");
 }
 
 exports.readActivity = (req, res, next) => {
+    console.log(activityService.readActivity(req));
     res.send("READ ACTIVITY!");
 }
 
 exports.updateActivity = (req, res, next) => {
+    console.log(activityService.updateActivity(req));
     res.send("UPDATE ACTIVITY");
 }
 
 exports.deleteActivity = (req, res, next) => {
+    console.log(activityService.deleteActivity(req));
     res.send("DELETE ACTIVITY");
 }
---
+++ service/activityService.js
@@ -0,0 +1,19 @@
+exports.createActivity = (id) => {
+    console.log(id);
+    return "CREATE ACTIVITY";
+}
+
+exports.readActivity = (id) => {
+    console.log(id);
+    return "READ ACTIVITY";
+}
+
+exports.updateActivity = (id) => {
+    console.log(id);
+    return "UPDATE ACTIVITY";
+}
+
+exports.deleteActivity = (id) => {
+    console.log(id);
+    return "DELETE ACTIVITY";
+}
