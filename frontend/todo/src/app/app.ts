0fc23599ddc591a0af459524e5d07a796e7ed6db
RiccardoParrino <riccardoparrino1@gmail.com>
2025-09-30 13:06:34 +0200

    adding some other style

--- frontend/todo/src/app/app.ts
+++ frontend/todo/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { Title } from './title/title';
 import { Activity } from './activity/activity';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Title, Activity],
+  imports: [RouterOutlet, Navbar, Title, Activity, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
