16f65db329ba17fa85b7be1d6c70fbd85efd1317
RiccardoParrino <riccardoparrino1@gmail.com>
2025-09-30 11:08:25 +0200

    adding all necessary component

---
+++ frontend/todo/src/app/activity-container/activity-container.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-container',
+  imports: [],
+  templateUrl: './activity-container.html',
+  styleUrl: './activity-container.css'
+})
+export class ActivityContainer {
+
+}
