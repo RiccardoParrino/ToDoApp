dae9bdbccf2fd467a44be6727b073ea1cfbd889e
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-01 11:51:53 +0200

    creating modal

--- frontend/todo/src/app/create-activity-modal/create-activity-modal.ts
+++ frontend/todo/src/app/create-activity-modal/create-activity-modal.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-create-activity-modal',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './create-activity-modal.css'
 })
 export class CreateActivityModal {
+  @Output() close = new EventEmitter<void>();
 
+  onClose() {
+    this.close.emit();
+  }
 }
--- frontend/todo/src/app/create-activity/create-activity.ts
+++ frontend/todo/src/app/create-activity/create-activity.ts
@@ -1,12 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CreateActivityModal } from '../create-activity-modal/create-activity-modal';
 
 @Component({
   selector: 'app-create-activity',
-  imports: [CommonModule],
+  imports: [CommonModule, CreateActivityModal],
   templateUrl: './create-activity.html',
   styleUrl: './create-activity.css'
 })
 export class CreateActivity {
-  
+  showModal:boolean = false;
+
+  openModal() : void {
+    console.log("opening modal");
+    this.showModal = true;
+  }
+
+  closeModal() : void {
+    console.log("closing modal");
+    this.showModal = false;
+  }
 }
