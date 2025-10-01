d9a2ad8be6dd754af8e5d3d9351c92ecf7afd72a
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-01 10:09:03 +0200

    remove create-activity-modal

--- frontend/todo/src/app/create-activity/create-activity.ts
+++ frontend/todo/src/app/create-activity/create-activity.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-activity',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './create-activity.html',
   styleUrl: './create-activity.css'
 })
 export class CreateActivity {
-
+  
 }
