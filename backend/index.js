6d1c8c7f4fd0018d73213ef6b08b7093e8d76641
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-02 14:50:58 +0200

    adding jwt as middleware function

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require("jsonwebtoken");
+const jwtUtilities = require('./middleware/jwtUtilities');
 const app = express();
 const port = 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use(jwtUtilities.verifyJwt);
 
 const activityRoutes = require('./routes/activityRoutes');
 const authenticationRoutes = require('./routes/authenticationRoutes');
-const jwtRoutes = require('./routes/jwtRoutes');
+const jwtRoutes = require('./routes/jwtRoutes')
 
 app.use('/auth',authenticationRoutes);
 app.use('/activity',activityRoutes);
-app.use('/jwt',jwtRoutes);
 
 app.listen(port, () => {
     console.log("Server listening at port 8080");
---
+++ backend/middleware/jwtUtilities.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+exports.createJwt = (email) => {
+    const token = jwt.sign(
+        { userId: email }, 
+        process.env.JWT_SECRET_KEY, 
+        { expiresIn: "2h" }
+    );
+    return token;
+}
+
+exports.verifyJwt = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1];
+
+    // jwt.verify( token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    //     if (err) {
+    //         console.log("Error occurred in JWT");
+    //         return false;
+    //     } else {
+    //         console.log("JWT is valid");
+    //         console.log(decoded.userId);
+    //         return true;
+    //     }
+    // });
+    
+    next();
+}
