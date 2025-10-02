c5b98ff1d80ea1bf95eeadbf5720c4f837e77f94
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-02 17:19:10 +0200

    add registration page

--- frontend/todo/src/app/app.routes.ts
+++ frontend/todo/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
+import { Registration } from './registration/registration';
 
 export const routes: Routes = [
     {
         path: 'login',
         component: Login,
         title: 'Login page'
+    },
+    {
+        path: 'registration',
+        component: Registration,
+        title: 'Registration page'
     }
 ];
