7a46c16b54e5de802caab7d25f8e6a7159dd3bc4
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-02 16:55:47 +0200

    adding routes in application

--- frontend/todo/src/app/app.ts
+++ frontend/todo/src/app/app.ts
@@ -5,13 +5,15 @@ import { Title } from './title/title';
 import { Activity } from './activity/activity';
 import { Footer } from './footer/footer';
 import { CreateActivity } from './create-activity/create-activity';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Title, Activity, Footer, CreateActivity],
+  imports: [RouterOutlet, Navbar, Title, Activity, Footer, CreateActivity, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('todo');
+  isLoggedIn:boolean = false;
 }
